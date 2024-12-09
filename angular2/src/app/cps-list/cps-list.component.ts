ce4d1bb29e3c7ab45c4f11c28be3ba80be914373
spapowW <uirandonnee@gmail.com>
2024-12-09 10:47:40 +0100

    h-issam

---
+++ angular2/src/app/cps-list/cps-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cps-list',
+  templateUrl: './cps-list.component.html',
+  styleUrl: './cps-list.component.css'
+})
+export class CpsListComponent {
+
+}
